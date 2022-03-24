7331c335615bb6669b44791416cd68601fc39307
Carolyn Webster-Pless <99274537+CarolynWP@users.noreply.github.com>
2022-03-24 14:47:31 +0000

    IT WORKSDGHFDGDHJ

--- js/script.js
+++ js/script.js
@@ -2,8 +2,9 @@ function enterClicked () {
   let name = document.getElementById("full-name").value
   let ssn = parseInt(document.getElementById("social-security").value)
 	let credit = parseInt(document.getElementById("credit-card").value)
-		let address = parseInt(document.getElementById("address").value)
-	let number = parseInt(document.getElementById("three-numbers").value)
+	let number = parseInt(document.getElementById("street-number").value)
+	let Stname = document.getElementById("street-name").value
+	let numbers = parseInt(document.getElementById("three-numbers").value)
   // display everything :)
-  document.getElementById('user-info').innerHTML = "Your full name is " + name + ", your social security number is " + ssn + ". You live at " + address + ", your credit card number is " + credit + " and those three little numbers on the back is " + number + ". Thank you so much for sharing! Wasn't that fun?"
+  document.getElementById('user-info').innerHTML = "Your full name is " + name + ", your social security number is " + ssn + ". You live at " + number + Stname + ", your credit card number is " + credit + " and those three little numbers on the back are " + numbers + ". Thank you so much for sharing! Wasn't that fun?"
 }
